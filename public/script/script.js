1248e6c139e96d479d84dd4faeeac4a158d86bb9
CarloJunio <48478168+CarloJunio@users.noreply.github.com>
2019-05-23 09:04:40 -0300

    Update script.js

--- public/script/script.js
+++ public/script/script.js
@@ -1,31 +1,47 @@
-function alphanumeric(inputOnlysix)
-{
- var letters = /\b[a-zA-Z0-9]{6}\b/;
- if(inputOnlysix.value.match(letters))
- {  
-    return true;     
-}
-else
-{
-   alert("message");
-   return false;
-}
-}
+// function alphanumeric(inputOnlysix)
+// {
+//  var letters = /\b[a-zA-Z0-9]{6}\b/;
+//  if(inputOnlysix.value.match(letters))
+//  {  
+//     return true;     
+// }
+// else
+// {
+//    alert("message");
+//    return false;
+// }
+// }
+$(document).ready(function(){
+    $('.submit').on('click', function(event) {
+    event.preventDefault();
+    var form = $(this).closest('form');
+    var formulario = form.serialize();
+    console.log('formulario',formulario);
+    $.ajax({
+        url: 'https://r2d2-secret-pass.herokuapp.com/validate',
+        type: 'get',
+        // This is the important part
+        xhrFields: {
+            withCredentials: true
+        },
+        // This is the important part
+        data: formulario,
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (xhr, status) {
+            // handle errors
+        }
 
-$('#submit').on('click', function(event) {
-  event.preventDefault();
-  var form = $(this).closest('form');
-  var formulario = form.serialize();
-  console.log('formulario',formulario);
-//   $.ajax({
-//     type:'GET',
-//     url:'https://r2d2-secret-pass.herokuapp.com/validate'
-//     data:formulario,
-//     dataType:'json',
-//     cache:false,
-//     success: function(data){
-//         console.log(data);
-//     }
-// });
-
+    // $.ajax({
+    //   type:'GET',
+    //   url:'https://r2d2-secret-pass.herokuapp.com/validate',
+    //   data:formulario,
+    //   dataType:'json',
+    //   cache:false,
+    //   success: function(data){
+    //       console.log(data);
+    //   }
+         });
+    });
 });
