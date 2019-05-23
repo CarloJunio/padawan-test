6efc1cbcd0df87a4ffc6e0637fe25f3ec4afd953
CarloJunio <48478168+CarloJunio@users.noreply.github.com>
2019-05-23 01:15:07 -0300

    Update script.js

--- public/script/script.js
+++ public/script/script.js
@@ -17,15 +17,15 @@ $('#submit').on('click', function(event) {
   var form = $(this).closest('form');
   var formulario = form.serialize();
   console.log('formulario',formulario);
-  $.ajax({
-    type:'GET',
-    url:'https://r2d2-secret-pass.herokuapp.com/validate'
-    data:formulario,
-    dataType:'json',
-    cache:false,
-    success: function(data){
-        console.log(data);
-    }
-});
+//   $.ajax({
+//     type:'GET',
+//     url:'https://r2d2-secret-pass.herokuapp.com/validate'
+//     data:formulario,
+//     dataType:'json',
+//     cache:false,
+//     success: function(data){
+//         console.log(data);
+//     }
+// });
 
 });
