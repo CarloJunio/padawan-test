af0c98bd13b1682ec2374f2d3178acab7ce72496
CarloJunio <48478168+CarloJunio@users.noreply.github.com>
2019-05-22 23:26:53 -0300

    Add files via upload

--- public/script/script.js
+++ public/script/script.js
@@ -3,8 +3,11 @@ function alphanumeric(inputOnlysix)
    var letters = /\b[a-zA-Z0-9]{6}\b/;
    if(inputOnlysix.value.match(letters))
      {
-      return true;
-     }
+        $.get( "https://r2d2-secret-pass.herokuapp.com/validate", function( data ) {
+          console.log(data);
+          alert( "Load was performed." );
+        });     
+      }
    else
      {
      alert("message");
