91baf2e7e69e5da9d46d6354ec9c18dbd9068ec8
CarloJunio <48478168+CarloJunio@users.noreply.github.com>
2019-05-23 23:41:31 -0300

    Update script.js

--- public/script/script.js
+++ public/script/script.js
@@ -1,40 +1,41 @@
-// onlick
+// onclick
 $(document).ready(function(){
     $('.submit').on('click', function(event) {
         event.preventDefault();
+        if (alphanumeric()) {
+            ajax();
+        }else
+        {
+            alert('ERROR');
+        }
 
     });
 });
 
 // alpha
-function alphanumeric(inputOnlysix)
-{
+function alphanumeric()
+{   var input = $("#pass").val();
     var letters = /\b[a-zA-Z0-9]{6}\b/;
-    if(inputOnlysix.value.match(letters))
+    if(input.match(letters))
     { 
 
         return true;     
-    }
-    else
-    {
-        alert ('ERROR');
-        return false;
     }
 }
 
 
 // ajax
 function ajax(){
-    var form = $(this).closest('form');
-    var formulario = form.serialize();
-    console.log('formulario',formulario);
-    $.ajax({
-      type:'GET',
-      url:'https://r2d2-secret-pass.herokuapp.com/validate',
-      data:formulario,
-      cache:false,
-      success: function(data){
-        window.location.href(data);
-    }
-});
+var form = $(this).closest('form');
+var formulario = form.serialize();
+console.log('formulario',formulario);
+$.ajax({
+  type:'GET',
+  url:'https://r2d2-secret-pass.herokuapp.com/validate',
+  data:formulario,
+  cache:false,
+  success: function(data){
+    window.location.replace(data);
+}
+}); 
 }
