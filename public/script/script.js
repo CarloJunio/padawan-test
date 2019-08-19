bd882f575c94412cb7a277a0ebc0e51a57d91d52
CarloJunio <48478168+CarloJunio@users.noreply.github.com>
2019-08-19 18:18:21 -0300

    Add files via upload

--- public/script/script.js
+++ public/script/script.js
@@ -1,55 +1,46 @@
-
 // onclick
-$(document).ready(function(){
-    $('.submit').on('click', function(event) {
+$(document).ready(function () {
+    document.getElementById("pass").onkeypress = function (e) {
+        var chr = String.fromCharCode(e.which);
+        if ("1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM-".indexOf(chr) < 0)
+            return false;
+    };
+    $('#form1').on('submit', function (event) {
         event.preventDefault();
         if (alphanumeric()) {
             ajax();
-        }else
-        {
-            alert('ACESSO INVÁLIDO');
-            location.reload();
+            return true;
+        } else {
+            alert("FORMATO INVÁLIDO");
         }
-
     });
-
-    $('#form1').on('submit', function(event) {
-        event.preventDefault();
-        if (alphanumeric()) {
-            ajax();
-        }else
-        {
-            alert('ACESSO INVÁLIDO');
-            location.reload();
-        }
-
-    });
-
 });
 
 // alpha
-function alphanumeric()
-{var input = $("#pass").val();
- var letters = /\b[a-zA-Z0-9]{6}\b/;
- if(input.length == 6 && input.match(letters))
- { 
-    return true;     
- }
+function alphanumeric() {
+    var input = $("#pass").val();
+    var letters = /^[a-zA-Z0-9.\-_]+$/;
+    if (input.length == 12 && input.match(letters)) {
+        return true;
+    }
 }
 
 
 // ajax
-function ajax(){
-    var form = $(this).closest('form');
-    var formulario = form.serialize();
-    console.log('formulario',formulario);
+function ajax() {
+    var formulario = $('#form1').serialize()
+    console.log('formulario', formulario);
     $.ajax({
-      type:'GET',
-      url:'https://r2d2-secret-pass.herokuapp.com/validate',
-      data:formulario,
-      cache:false,
-      success: function(data){
-        window.location.replace(data);
-    }
-}); 
+        type: 'GET',
+        url: 'http://www.pennawd.com.br/webservice.php',
+        data: formulario,
+        dataType: 'JSON',
+        success: function (data) {
+            console.log(data);
+            swal("COMPLETE!","Authorization:" + data.authorization + "\nMessage:" + data.message,"success");
+        },
+        error: function () {
+            alert("SENHA INVALIDA");
+        }
+    });
 }
